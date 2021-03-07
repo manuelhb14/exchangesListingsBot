34a78a750e1917831d598b5c7145d850fbec21cc
manuelhb14 <mhernandezb96@gmail.com>
2021-03-06 21:18:34 -0600

    add listener for specific user, tested with own tweets, 15 sec delay approx

---
+++ binance_bot.py
@@ -0,0 +1,29 @@
+from tweepy import OAuthHandler, Stream, StreamListener
+import keys
+
+consumer_key=keys.consumer_key
+consumer_secret=keys.consumer_secret
+
+access_token=keys.access_token
+access_token_secret=keys.access_token_secret
+
+class StdOutListener(StreamListener):
+    """ A listener handles tweets that are received from the stream.
+    This is a basic listener that just prints received tweets to stdout.
+    """
+    def on_data(self, data):
+        print(data)
+        return True
+
+    def on_error(self, status):
+        print(status)
+
+if __name__ == '__main__':
+    l = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    stream = Stream(auth, l)
+    stream.filter(follow=['829941007076687872'])
+
+    
