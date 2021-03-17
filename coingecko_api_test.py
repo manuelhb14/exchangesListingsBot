dc6ebcfa51e6b0f555315d8f9a74db99d704ade2
manuelhb14 <mhernandezb96@gmail.com>
2021-03-17 00:28:37 -0600

    add all possible coins with same symbol and get all its possible chains in test py

--- coingecko_api_test.py
+++ coingecko_api_test.py
@@ -1,35 +1,26 @@
 from pycoingecko import CoinGeckoAPI
 from requests import adapters
 cg = CoinGeckoAPI()
-
 coinlist = cg.get_coins_list()
-# print(len(coinlist))
-coin_symbol = 'bifi'
-for i in coinlist:
-    if i['symbol'] == coin_symbol:
-        coin_info = i
-        print(coin_info)
-contract_address = cg.get_coin_by_id(coin_info['id'])
-# contract_address_eth = contract_address["platforms"]['ethereum']
-contract_address_bsc = contract_address
-coin_price = contract_address["market_data"]["current_price"]["usd"]
-print(contract_address_bsc)
-# print(contract_address.keys())
-# print(contract_address)
-# for key in contract_address.keys():
-#     print(contract_address[key])
 
-# seen = {}
-# dupes = []
+coins_info = []
+coins_details = []
+coins_contract_address = []
+con_num = 0
+coin_symbol = 'super'
 
-# for x in coinlist:
-#     if x['symbol'] not in seen:
-#         seen[x['symbol']] = 1
-#     else:
-#         if seen[x['symbol']] == 1:
-#             dupes.append(x)
-#         seen[x['symbol']] += 1
-# print('\nlist of duplicated ids')
-# for i in dupes:
-#     print(i['symbol'],  end='', '')
-# print()
+while con_num<len(coinlist):
+    if coinlist[con_num]['symbol'] == coin_symbol:
+        coins_info.append(coinlist[con_num])
+    con_num+=1
+
+for i in coins_info:
+    coins_details.append(cg.get_coin_by_id(i['id']))
+
+for i in coins_details:
+    print(i["links"]["twitter_screen_name"])
+    print(i["platforms"])
+
+# contract_address_bsc = contract_address
+# coin_price = contract_address["market_data"]["current_price"]["usd"]
+# print(contract_address_bsc)
