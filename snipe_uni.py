1c1c975b1624479a18ff7b3c3c106bc426c685a7
manuelhb14 <mhernandezb96@gmail.com>
2021-04-24 01:04:38 -0500

    add programs to 'snipe' new listings on uniswap and panacake swap, need more testing

--- snipe_uni.py
+++ snipe_uni.py
@@ -2,10 +2,10 @@ from txns import Txn_bot
 
 token_address = ""
 
-quantity = 0.13*10**18
+quantity = 1*10**18
 net = 'eth-mainnet'
 slippage = 30 #%
-gas_price = 160*10**9 #Gwei, bsc-mainnet=5, eth-mainnet=https://www.gasnow.org/, eth-rinkeby=1
+gas_price = 40*10**9 #Gwei, bsc-mainnet=5, eth-mainnet=https://www.gasnow.org/, eth-rinkeby=1
 bot = Txn_bot(token_address, quantity, net, slippage, gas_price)
 tokens = bot.get_amounts_out_buy()
 while (tokens==0):
